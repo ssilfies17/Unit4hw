d9e59989edad356934648b80dd2431f6630f65a5
Sawyer Silfies <ssilfies17@gmail.com>
2021-12-08 17:58:22 -0800

    primmary html page setup

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 var pgraph = document.querySelector("p");
 var h1 = document.querySelector("h1");
+var button = document.querySelector(".button");
 
-h1.setAttribute()
+h1.setAttribute("style", "font-size:42px; padding-top:50px; margin:20px; text-align:center;");
+pgraph.setAttribute("style", "font-size:18px; text-align:center; padding:20px; padding-bottom:50px; margin:10px;");
+button.setAttribute("style", "text-align:center;")
+button.querySelector("button").setAttribute("style", "padding-left:40px; padding-right:40px; padding-top:10px; padding-bottom:10px;")
+
+
+
+
+
+console.log("hello");
