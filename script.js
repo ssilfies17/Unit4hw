12ef13c077e22d5d177c9ee88592aca7b9747bff
Sawyer Silfies <ssilfies17@gmail.com>
2021-12-21 14:50:42 -0800

    fixed variables

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var pgraph = document.querySelector("p");
-var h1 = document.querySelector("h1");
+var startInfo = document.querySelector("#gameInfo");
+var startHeader = document.querySelector("#startHeader");
 var button = document.querySelector(".button");
 
-h1.setAttribute("style", "font-size:42px; padding-top:50px; margin:20px; text-align:center;");
-pgraph.setAttribute("style", "font-size:18px; text-align:center; padding:20px; padding-bottom:50px; margin:10px;");
+startHeader.setAttribute("style", "font-size:42px; padding-top:50px; margin:20px; text-align:center;");
+startInfo.setAttribute("style", "font-size:18px; text-align:center; padding:20px; padding-bottom:50px; margin:10px;");
 button.setAttribute("style", "text-align:center;")
 button.querySelector("button").setAttribute("style", "padding-left:40px; padding-right:40px; padding-top:10px; padding-bottom:10px;")
 
